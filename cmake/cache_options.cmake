79dd0ade512eea0f45ac39b3279b37e863b6a0cf
Orell Garten <orellgarten@gmx.de>
2020-07-21 20:45:57 +0200

    added needed options

--- cmake/cache_options.cmake
+++ cmake/cache_options.cmake
@@ -1,5 +1,3 @@
-OPTION(MYPROJECT_VAR1 "Boolen project variable" ON)
-SET(MYPROJECT_VAR2 "" CACHE STRING "string project variable")
-
+OPTION(BUILD_SERVER "Build server" ON)
+OPTION(BUILD_CLIENT "Build client" ON)
 OPTION(BUILD_TEST "Build test cases" ON)
-OPTION(BUILD_BENCHMARK "Build benchmarks cases" ON)
