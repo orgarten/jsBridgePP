28141b400f34cf93bca116fe49c70359e9f545d9
Orell Garten <orellgarten@gmx.de>
2020-08-08 11:30:38 +0200

    moved client implementation to library

--- apps/client/main.cpp
+++ apps/client/main.cpp
@@ -1,85 +1,13 @@
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
-
 #include <iostream>
 
-typedef websocketpp::client<websocketpp::config::asio_client> client;
-
-using websocketpp::lib::placeholders::_1;
-using websocketpp::lib::placeholders::_2;
-using websocketpp::lib::bind;
-
-// pull out the type of messages sent by our config
-typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
-
-// This handler will be invoked once the connection to the server is opened
-// after the initial messages (registration + hello) all incoming messages are being echoed back
-void on_open(client* c, websocketpp::connection_hdl hdl) {
-    // register as the game
-    std::string msg = "game";
-    c->send(hdl,msg,websocketpp::frame::opcode::text);
-    c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
-    
-    // send some data to the user via the bridge server
-    msg = "Hi from game!";
-    c->send(hdl,msg,websocketpp::frame::opcode::text);
-    c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
-}
-
-// This message handler will be invoked once for each incoming message. It
-// prints the message and then sends a copy of the message back to the server.
-void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
-    std::cout << "on_message called with hdl: " << hdl.lock().get()
-              << " and message: " << msg->get_payload()
-              << std::endl;
+#include "../../jsBridge++/client/Client.h"
 
 
-    websocketpp::lib::error_code ec;
-
-    c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
-    if (ec) {
-        std::cout << "Echo failed because: " << ec.message() << std::endl;
-    }
-}
 
 int main(int argc, char* argv[]) {
-    // Create a client endpoint
-    client c;
+    Client client("ws://localhost:9002");
 
-    std::string uri = "ws://localhost:9002";
+    auto res = client.run();
 
-    if (argc == 2) {
-        uri = argv[1];
-    }
-
-    try {
-        // Set logging to be pretty verbose (everything except message payloads)
-        c.set_access_channels(websocketpp::log::alevel::all);
-        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-        // Initialize ASIO
-        c.init_asio();
-
-        // Register our handlers
-        c.set_open_handler(bind(&on_open,&c,::_1));
-        c.set_message_handler(bind(&on_message,&c,::_1,::_2));
-
-        websocketpp::lib::error_code ec;
-        client::connection_ptr con = c.get_connection(uri, ec);
-        if (ec) {
-            std::cout << "could not create connection because: " << ec.message() << std::endl;
-            return 0;
-        }
-
-        // Note that connect here only requests a connection. No network messages are
-        // exchanged until the event loop starts running in the next line.
-        c.connect(con);
-
-        // Start the ASIO io_service run loop
-        // this will cause a single connection to be made to the server. c.run()
-        // will exit when this connection is closed.
-        c.run();
-    } catch (websocketpp::exception const & e) {
-        std::cout << e.what() << std::endl;
-    }
+    return res;
 }
--- jsBridge++/client/Client.h
+++ jsBridge++/client/Client.h
@@ -5,11 +5,91 @@
 #ifndef JSBRIDGE_JSBRIDGE_LIB_CLIENT_CLIENT_H_
 #define JSBRIDGE_JSBRIDGE_LIB_CLIENT_CLIENT_H_
 
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+
 #include <iostream>
+#include <utility>
+
+using client = websocketpp::client<websocketpp::config::asio_client>;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+// pull out the type of messages sent by our config
+using message_ptr = websocketpp::config::asio_client::message_type::ptr;
+
+// This handler will be invoked once the connection to the server is opened
+// after the initial messages (registration + hello) all incoming messages are being echoed back
+void on_open(client* c, websocketpp::connection_hdl hdl) {
+  // register as the game
+  std::string msg = "game";
+  c->send(hdl,msg,websocketpp::frame::opcode::text);
+  c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
+
+  // send some data to the user via the bridge server
+  msg = "Hi from game!";
+  c->send(hdl,msg,websocketpp::frame::opcode::text);
+  c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
+}
+
+// This message handler will be invoked once for each incoming message. It
+// prints the message and then sends a copy of the message back to the server.
+void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
+  std::cout << "on_message called with hdl: " << hdl.lock().get()
+            << " and message: " << msg->get_payload()
+            << std::endl;
+
+
+  websocketpp::lib::error_code ec;
+
+  c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
+  if (ec) {
+    std::cout << "Echo failed because: " << ec.message() << std::endl;
+  }
+}
 
 class Client {
 public:
-  Client() { std::cout << "-> Constructed client! \n"; }
+  std::string uri;
+  client cl = client();
+
+  explicit Client(const std::string uri) : uri(uri) {  }
+
+  int run() {
+    try {
+      // Set logging to be pretty verbose (everything except message payloads)
+      cl.set_access_channels(websocketpp::log::alevel::all);
+      cl.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+      // Initialize ASIO
+      cl.init_asio();
+
+      // Register our handlers
+      cl.set_open_handler(bind(&on_open,&cl,::_1));
+      cl.set_message_handler(bind(&on_message,&cl,::_1,::_2));
+
+      websocketpp::lib::error_code ec;
+      client::connection_ptr con = cl.get_connection(uri, ec);
+      if (ec) {
+        std::cout << "could not create connection because: " << ec.message() << std::endl;
+        return -1;
+      }
+
+      // Note that connect here only requests a connection. No network messages are
+      // exchanged until the event loop starts running in the next line.
+      cl.connect(con);
+
+      // Start the ASIO io_service run loop
+      // this will cause a single connection to be made to the server. c.run()
+      // will exit when this connection is closed.
+      cl.run();
+    } catch (websocketpp::exception const & e) {
+      std::cout << e.what() << std::endl;
+    }
+    return 0;
+  }
 };
 
 #endif//JSBRIDGE_JSBRIDGE_LIB_CLIENT_CLIENT_H_
